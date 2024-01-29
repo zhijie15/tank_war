c082f1ad803e36cc736baeed272f2017d07bc9a8
zhijie15 <119439275+zhijie15@users.noreply.github.com>
2024-01-29 15:27:49 +0100

    Create c2.png

--- tank_war.py
+++ tank_war.py
@@ -1,19 +1,22 @@
 import pygame
+import os
 FPS=30
 WIDTH=1550
 HEIGHT=780
 WHITE=(255,255,255)
-GREEN=(0,255,0)
-中春绿色=(0,250,154)
+BLACK=(0,0,0)
 pygame.init()
 screen=pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("坦克大战")
 clock=pygame.time.Clock()
 runing=True
 class Player(pygame.sprite.Sprite):
     def __init__(self) -> None:
         super().__init__()
-        self.image=pygame.Surface((25,25))
-        self.image.fill(中春绿色)
+        tank_img=pygame.image.load(os.path.join("pic","tank_sprite.png")).convert()
+        self.tank=tank_img
+        self.tank.set_colorkey(WHITE)
+        self.image=self.tank.subsurface((0,0),(34,34))
         self.rect=self.image.get_rect()
         self.rect.centerx=WIDTH/2
         self.rect.bottom=HEIGHT
@@ -22,12 +25,16 @@ class Player(pygame.sprite.Sprite):
         key_pressed =pygame.key.get_pressed()
         if key_pressed[pygame.K_RIGHT]:
             self.rect.x+=self.speed
+            self.image=self.tank.subsurface((66,0),(34,34))
         if key_pressed[pygame.K_LEFT]:
             self.rect.x-=self.speed
+            self.image=self.tank.subsurface((200,0),(34,34))
         if key_pressed[pygame.K_UP]:
             self.rect.y-=self.speed
+            self.image=self.tank.subsurface((0,0),(34,34))
         if key_pressed[pygame.K_DOWN]:
             self.rect.y+=self.speed
+            self.image=self.tank.subsurface((135,0),(34,34))
         if(self.rect.right>WIDTH):
             self.rect.right=WIDTH
         if(self.rect.left<0):
@@ -44,7 +51,7 @@ while runing:
     for even in pygame.event.get():
         if even.type==pygame.QUIT:
             runing=False
-    screen.fill(WHITE)
+    screen.fill(BLACK)
     all_sprites.update()
     all_sprites.draw(screen)
     pygame.display.update()
